87fd9041ca28b9f7fc9f915f3ac474104b8dc146
ATSTNG <atstng@gmail.com>
2024-06-26 20:30:40 +0400

    Updated to version 14.13.1

--- main.js
+++ main.js
@@ -542,8 +542,8 @@ class LeagueStatsTable {
     }
 }
 
-var LEAGUE_VERSION = "14.10.1";
-var LAST_UPDATED = "19 May 2024";
+var LEAGUE_VERSION = "14.13.1";
+var LAST_UPDATED = "26 June 2024";
 var MAX_GOLD = 150 * 1000;
 var MAX_TIME = 240 * 60;
 var itemset = new LeagueItemSet();
